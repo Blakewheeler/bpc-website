da04525c6e92d8b38dc305cbd9ed7940ba189e8a
Cyber Ghost <228818737+Blakewheeler@users.noreply.github.com>
2026-09-04 04:30:40 -0400

    Initialize builds data with multiple entries
    Added an array of build objects with IDs, names, and images.

---
+++ builds-data.js
@@ -0,0 +1,132 @@
+const builds = [
+  {
+    id: "build-01",
+    name: "Custom White Dump Truck",
+    images: [
+      "images/build-01/build-01-01.jpeg",
+      "images/build-01/build-01-02.jpeg",
+      "images/build-01/build-01-03.jpeg",
+      "images/build-01/build-01-04.jpeg",
+      "images/build-01/build-01-05.jpeg",
+      "images/build-01/build-01-06.jpeg",
+      "images/build-01/build-01-07.jpeg",
+      "images/build-01/build-01-08.jpeg",
+      "images/build-01/build-01-09.jpeg",
+      "images/build-01/build-01-10.jpeg"
+    ]
+  },
+
+  {
+    id: "build-02",
+    name: "Custom Build 02",
+    images: []
+  },
+
+  {
+    id: "build-03",
+    name: "Custom Build 03",
+    images: []
+  },
+
+  {
+    id: "build-04",
+    name: "Custom Build 04",
+    images: []
+  },
+
+  {
+    id: "build-05",
+    name: "Custom Build 05",
+    images: []
+  },
+
+  {
+    id: "build-06",
+    name: "Custom Build 06",
+    images: []
+  },
+
+  {
+    id: "build-07",
+    name: "Custom Build 07",
+    images: []
+  },
+
+  {
+    id: "build-08",
+    name: "Custom Build 08",
+    images: []
+  },
+
+  {
+    id: "build-09",
+    name: "Custom Build 09",
+    images: []
+  },
+
+  {
+    id: "build-10",
+    name: "Custom Build 10",
+    images: []
+  },
+
+  {
+    id: "build-11",
+    name: "Custom Build 11",
+    images: []
+  },
+
+  {
+    id: "build-12",
+    name: "Custom Build 12",
+    images: []
+  },
+
+  {
+    id: "build-13",
+    name: "Custom Build 13",
+    images: []
+  },
+
+  {
+    id: "build-14",
+    name: "Custom Build 14",
+    images: []
+  },
+
+  {
+    id: "build-15",
+    name: "Custom Build 15",
+    images: []
+  },
+
+  {
+    id: "build-16",
+    name: "Custom Build 16",
+    images: []
+  },
+
+  {
+    id: "build-17",
+    name: "Custom Build 17",
+    images: []
+  },
+
+  {
+    id: "build-18",
+    name: "Custom Build 18",
+    images: []
+  },
+
+  {
+    id: "build-19",
+    name: "Custom Build 19",
+    images: []
+  },
+
+  {
+    id: "build-20",
+    name: "Custom Build 20",
+    images: []
+  }
+];
